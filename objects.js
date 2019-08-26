9d7d0f7bbffe075d549f1a6603176efd3e131bf6
Nick Spera <nickaspera@gmail.com>
2019-08-26 02:25:41 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,8 +1,9 @@
 // var playlist = {artistName: 'songTitle' }
 var playlist = {
   // playlist: 'Slowdive',
-  artistName: 'Phil Ochs',
-  songTitle: 'My Bloody Valentine'
+  // artistName: 'Phil Ochs',
+  // songTitle: 'My Bloody Valentine'
+  'Phil Ochs': 'My Bloody Valentine'
 }
 
 function updatePlaylist(playlist, artistName, songTitle){
